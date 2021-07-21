50fb0457b25ec6d706a7d45f923e67f287f8c31a
Dogl4 <doougllas@hotmail.com.br>
2021-07-21 18:42:09 -0300

    18° Requisito - Adiciona um evento no scrip.js,que o botao enviar so estara ativo quando o checkbox for marcado.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const botao = document.getElementById('btn-logar');
 const login = document.getElementById('login');
 const senha = document.getElementById('senha');
+const botaoEnviar = document.querySelector('#submit-btn');
+const termos = document.querySelector('#agreement');
 
 botao.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Login ou senha inválidos.');
+  }
+});
+
+termos.addEventListener('click', () => {
+  if (termos.checked) {
+    botaoEnviar.disabled = false;
+  } else {
+    botaoEnviar.disabled = true;
   }
 });
