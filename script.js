27e4695bb304d9490afda1cafea18cbf24f50746
Dogl4 <doougllas@hotmail.com.br>
2021-07-24 17:53:46 -0300

    21° Requisito - CONCLUIDO!, conserta a funçao que obtem o radio seleciona, junto com problemas de escopo.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const nome = document.getElementById('input-name');
 const sobrenome = document.getElementById('input-lastname');
 const email = document.getElementById('input-email');
 const casa = document.getElementById('house');
-const nota = document.getElementsByName('rate');
-const familia = document.getElementsByName('family');
+// const nota = document.querySelectorAll('.nota');
+// const familia = document.querySelectorAll('.familia');
 const texto = document.getElementById('textarea');
 const skills = document.querySelectorAll('.subject');
 
@@ -37,54 +37,12 @@ texto.addEventListener('input', () => {
   }
 });
 
-// // Função verifica qual radio está selecionado, logo após, já o guarda
-// const guardaRadioSelecionado = (array, nomeChave) => {
-//   for (let index = 0; index < array.length; index += 1) {
-//     if (array[index].checked) {
-//       localStorage.setItem(`${nomeChave}:`, array[index].value);
-//     }
-//   }
-// };
-
-// // const checkboxSeleceted = (array) => {
-
-// // };
-
-// // Requisito 21:
-// botaoEnviar.addEventListener('click', () => {
-//   localStorage.clear();
-//   localStorage.setItem('Nome:', `${nome.value} ${sobrenome.value}`);
-//   localStorage.setItem('Email:', email.value);
-//   localStorage.setItem('Casa:', casa.value);
-//   guardaRadioSelecionado(familia, 'Família');
-//   guardaRadioSelecionado(skills, 'Matérias');
-//   guardaRadioSelecionado(avalia, 'Avaliação');
-//   localStorage.setItem('Observações:', texto.value);
-//   // localStorage.setItem('Família:', familia.value);
-//   while (form.firstChild) {
-//     form.removeChild(form.firstChild);
-//   }
-//   for (let index = 0; index < localStorage.length; index += 1) {
-//     const text = document.createElement('p');
-//     text.innerText = `${localStorage.key(index)} ${localStorage.getItem(localStorage.key(index))}`;
-//     form.appendChild(text);
-//   }
-// });
-
-// // Função verifica qual radio está selecionado
-// function valueRadio(array) {
-//   for (let i = 0; i < array.length; i += 1) {
-//     if (array[i].checked) {
-//       return array[i].valeu;
-//     }
-//   }
-// }
-
 // Função verifica qual radio está selecionado, logo após, já o guarda
-const guardaRadioSelecionado = (array, nomeChave) => {
+const guardaRadioSelecionado = (nameEntreAspas, nomeChave) => {
+  const array = document.getElementsByName(`${nameEntreAspas}`);
   for (let index = 0; index < array.length; index += 1) {
     if (array[index].checked) {
-      localStorage.setItem(`${nomeChave}:`, array[index].value);
+      return `<p>${nomeChave}: ${array[index].value} </p>`;
     }
   }
 };
@@ -102,16 +60,14 @@ const guardaCheckboxSelecionado = (array, nomeChave) => {
 
 // Requisito 21:
 botaoEnviar.addEventListener('click', () => {
-  guardaRadioSelecionado(familia, 'Família');
-  guardaRadioSelecionado(nota, 'Avaliação');
-
-  localStorage.clear();
+  const familia = guardaRadioSelecionado('family', 'Família'); // Guarda os dados na variavel, antes de resetar o forms.
+  const nota = guardaRadioSelecionado('rate', 'Avaliação');
   form.innerHTML = '';
   form.innerHTML += `<p>Nome: ${nome.value} ${sobrenome.value}</p>`;
   form.innerHTML += `<p>Email: ${email.value}</p>`;
   form.innerHTML += `<p>Casa: ${casa.value}</p>`;
-  // form.innerHTML += `<p>Família: ${localStorage.getItem('Família')}</p>`;
+  form.innerHTML += familia;
   form.innerHTML += `<p>${guardaCheckboxSelecionado(skills, 'Matérias')}</p>`;
-  // form.innerHTML += `<p>Avaliação: ${localStorage.getItem('Avaliação')}</p>`;
+  form.innerHTML += nota;
   form.innerHTML += `<p>Observações: ${texto.value}</p>`;
 });
